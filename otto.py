9c46760bac39e7784c1ea0498c58060f9ae103b4
jgy <jgy2001@126.com>
2024-07-17 21:58:12 +0800

    a

--- otto.py
+++ otto.py
@@ -93,12 +93,10 @@ def word2pinyin(word: str):
 
 
 if __name__ == '__main__':
-    #word='大家好 我是里铁 我是承德土皇帝'
-   # f=
+    
     word="它的优点在于计算的范围取决于箱子容量。可以计算大树乘法。谢谢大家的观看。"
-    #word = "下面我简单介绍一下工作原理。通过使用肉斗和侦车器。第一个箱子每输出一次，第二个箱子输出全部物品。再通过弹射置物台返回箱子。再由一个侦车器负责计数和保存结果。"
-    # word='差不多得了,屁大点事都要拐上原神，原神一没招你惹你，二没干伤天害理的事情，到底怎么你了让你一直无脑抹黑，米哈游每天费尽心思的文化输出弘扬中国文化，你这种喷子只会在网上敲键盘诋毁良心公司，中国游戏的未来就是被你这种人毁掉的'
+    
     bing = pinyin2otto(word2pinyin(word))
-    bing.export('./output/output6.wav', format='wav')
+    bing.export('./output/output.wav', format='wav')
 
-    # playsound('./output/output.wav')
+    
